e33a2e971ccd2e87594dfa4523f360b640e1c061
unsivil <unsivilaudio@gmail.com>
2020-10-11 09:30:23 -0600

    refactor application to more modern structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Board from '../containers/Board';
+import '../assets/stylesheets/app.css';
+
+const app = props => {
+    return (
+        <div className='App'>
+            <Board />
+        </div>
+    );
+};
+
+export default app;
---
+++ src/components/Cell.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../assets/stylesheets/cell.css';
+
+/** A single cell on the board.
+ *
+ * This has no state --- just two props:
+ *
+ * - flipCellsAroundMe: a function rec'd from the board which flips this
+ *      cell and the cells around of it
+ *
+ * - isLit: boolean, is this cell lit?
+ *
+ * This handles clicks --- by calling flipCellsAroundMe
+ *
+ **/
+
+const cell = props => {
+    const handleClick = e => {
+        // call up to the board to flip cells around this cell
+        this.props.flipCellsAroundMe();
+    };
+
+    let classes = 'Cell' + (props.isLit ? ' Cell-lit' : '');
+
+    return <td className={classes} onClick={handleClick} />;
+};
+
+export default cell;
